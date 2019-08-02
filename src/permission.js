3821a9d32c277c79be558bf89f8da007fd533f3d
yangzx <597032797@qq.com>
2019-08-02 14:12:49 +0800

    不判断登录

--- src/permission.js
+++ src/permission.js
@@ -58,14 +58,14 @@ router.beforeEach(async(to, from, next) => {
   } else {
     /* has no token*/
 
-    if (whiteList.indexOf(to.path) !== -1) {
+    // if (whiteList.indexOf(to.path) !== -1) {
       // in the free login whitelist, go directly
       next()
-    } else {
+    // } else {
       // other pages that do not have permission to access are redirected to the login page.
-      next(`/login?redirect=${to.path}`)
-      NProgress.done()
-    }
+    //   next(`/login?redirect=${to.path}`)
+    //   NProgress.done()
+    // }
   }
 })
 
