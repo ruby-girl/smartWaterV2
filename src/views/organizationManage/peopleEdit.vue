0d225a905c8a6fbe0a1d4af8f3376af7596246f9
chenli <814609761@qq.com>
2019-08-09 10:11:54 +0800

    组织管理

--- src/views/organizationManage/peopleEdit.vue
+++ src/views/organizationManage/peopleEdit.vue
@@ -1,105 +1,105 @@
 <template>
   <div class="cl-container cl-container3">
     <div>
-      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form ref="ruleForm" :model="ruleForm" :rules="rules" label-width="100px">
         <el-row :gutter="50">
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="人员编号:">
-              <el-input v-model="ruleForm.code" size="small" disabled></el-input>
+              <el-input v-model="ruleForm.code" size="small" disabled />
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="人员名称:" prop="name">
-              <el-input v-model="ruleForm.name" size="small" maxlength="10" placeholder="长度10"></el-input>
+              <el-input v-model="ruleForm.name" size="small" maxlength="10" placeholder="长度10" />
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="公司:" prop="company">
               <el-select v-model="ruleForm.company" placeholder="请选择（单选）" size="small">
-                <el-option label="区域一" value="shanghai"></el-option>
-                <el-option label="区域二" value="beijing"></el-option>
+                <el-option label="区域一" value="shanghai" />
+                <el-option label="区域二" value="beijing" />
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="水厂:" prop="waterworks">
               <el-select v-model="ruleForm.waterworks" placeholder="请选择（单选）" size="small">
-                <el-option label="区域一" value="shanghai"></el-option>
-                <el-option label="区域二" value="beijing"></el-option>
+                <el-option label="区域一" value="shanghai" />
+                <el-option label="区域二" value="beijing" />
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="部门:" prop="department">
               <el-select v-model="ruleForm.department" placeholder="请选择（单选）" size="small">
-                <el-option label="区域一" value="shanghai"></el-option>
-                <el-option label="区域二" value="beijing"></el-option>
+                <el-option label="区域一" value="shanghai" />
+                <el-option label="区域二" value="beijing" />
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="岗位:" prop="post">
               <el-select v-model="ruleForm.post" placeholder="请选择（单选）" size="small">
-                <el-option label="区域一" value="shanghai"></el-option>
-                <el-option label="区域二" value="beijing"></el-option>
+                <el-option label="区域一" value="shanghai" />
+                <el-option label="区域二" value="beijing" />
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="性别:">
               <el-select v-model="ruleForm.sex" placeholder="请选择（单选）" size="small">
-                <el-option label="女" value="女"></el-option>
-                <el-option label="男" value="男"></el-option>
+                <el-option label="女" value="女" />
+                <el-option label="男" value="男" />
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="出生日期:">
-              <el-date-picker type="date" placeholder="年月日（单选）" v-model="ruleForm.birthday" size="small"></el-date-picker>
+              <el-date-picker v-model="ruleForm.birthday" type="date" placeholder="年月日（单选）" size="small" />
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="电话号码:" prop="phone">
-              <el-input v-model="ruleForm.phone" size="small" maxlength="11" placeholder="长度11"></el-input>
+              <el-input v-model="ruleForm.phone" size="small" maxlength="11" placeholder="长度11" />
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="身份证号:" prop="id">
-              <el-input v-model="ruleForm.id" size="small" maxlength="18" placeholder="长度18"></el-input>
+              <el-input v-model="ruleForm.id" size="small" maxlength="18" placeholder="长度18" />
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="入职时间:">
-              <el-date-picker type="date" placeholder="年月日（单选）" v-model="ruleForm.entryTime" size="small"></el-date-picker>
+              <el-date-picker v-model="ruleForm.entryTime" type="date" placeholder="年月日（单选）" size="small" />
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="岗位状态:">
-              <el-input v-model="ruleForm.status" size="small" disabled></el-input>
+              <el-input v-model="ruleForm.status" size="small" disabled />
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
             <el-form-item label="地址:" prop="adress">
-              <el-input type="textarea" v-model="ruleForm.adress" size="small" maxlength="100" placeholder="长度100"></el-input>
+              <el-input v-model="ruleForm.adress" type="textarea" size="small" maxlength="100" placeholder="长度100" />
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="12" :md="8" :lg="8" :xl="8">
             <el-form-item label="附件:">
               <el-select v-model="ruleForm.certificates" placeholder="请选择（单选）" size="small">
-                <el-option label="身份证" value="身份证"></el-option>
+                <el-option label="身份证" value="身份证" />
               </el-select>
             </el-form-item>
           </el-col>
           <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24">
             <el-form-item>
-              <uploadImg @getFileFun="getFileFun"></uploadImg>
+              <uploadImg @getFileFun="getFileFun" />
             </el-form-item>
           </el-col>
         </el-row>
         <p class="footBox">
           <el-checkbox v-model="goOn">继续添加</el-checkbox>
-          <el-button type="primary" @click="submitForm('ruleForm')" size="small">保存</el-button>
-          <el-button @click="resetForm('ruleForm')" size="small">取消</el-button>
+          <el-button type="primary" size="small" @click="submitForm('ruleForm')">保存</el-button>
+          <el-button size="small" @click="resetForm('ruleForm')">取消</el-button>
         </p>
       </el-form>
     </div>
@@ -189,7 +189,6 @@ export default {
         if (valid) {
           alert('submit!')
         } else {
-          console.log('error submit!!')
           return false
         }
       })
