05913a3019400e0824a66a6061c2c0a19a79d10f
chenli <814609761@qq.com>
2019-10-14 15:30:59 +0800

    抄表设置

--- src/views/businessManagement/meterSetUp/components/MeterPlan.vue
+++ src/views/businessManagement/meterSetUp/components/MeterPlan.vue
@@ -23,7 +23,7 @@
           </div>
           <div class="meter_box meter_box3">
             <p>本次读书</p>
-            <el-input v-model="param.ReadNum" placeholder="按[enter]键确定" @keyup.enter.native="getWaterPredict()" onkeyup="value=value.replace(/\D/g,'')"></el-input>
+            <el-input v-model="param.ReadNum" placeholder="按[enter]键确定" @keyup.enter.native="getWaterPredict(1)" onkeyup="value=value.replace(/\D/g,'')"></el-input>
           </div>
           <el-input class="meter_remark" placeholder="请输入内容" v-model="param.Remark">
               <template slot="prepend">备注：</template>
@@ -43,7 +43,7 @@
               </el-checkbox-group>
             </el-col>
             <el-col :span="4">
-              <el-button style="float: right" type="primary" size="small" @click="getCheck()">确定</el-button>
+              <el-button style="float: right" type="primary" size="small" @click="getWaterPredict(2)">确定</el-button>
             </el-col>
           </el-row>
       </div>
@@ -125,14 +125,19 @@
           Remark : '' ,
           IsPage : false
         },
-        magnification:''//倍率
+        magnification:'1.5',//倍率,
+        waterPriceData:{}
       }
     },
     methods:{
-      getCheck(){//抄表
-        getReading(this.param).then(res => {
+      getCheck(param){//抄表
+        getReading(param).then(res => {
           if (res.code ==0 ) {
-            console.log(res)
+            this.$message({
+              message: res.message,
+              type: 'warning',
+              duration: 4000
+            });
           } else {
             this.$message({
               message: res.message,
@@ -142,10 +147,14 @@
           }
         })
       },
-      getWaterPredict(){//获取水量水费数据
+      getWaterPredict(type){//获取水量水费数据,type为1时候请求水费水量预估接口，为2时候请求抄表接口
+        this.param.SA_MeterRecord_Id = this.currentContract.Id
         if(typeof(this.currentContract.CustomerNo)!="undefined"&&this.currentContract.CustomerNo !=''){
           let curNum = this.param.ReadNum//本次读书
           if(curNum !=''){
+            let params = Object.assign({}, this.param);//赋值对象转换参数类型
+            params.ReadNum !='' ? params.ReadNum = parseInt(params.ReadNum) : ''
+
             if(this.checks.indexOf('字轮是否翻页')=='-1'){//当未勾选字轮翻页,判断本次读书必须大于上次读书
               this.param.IsPage = false
               if(this.currentContract.LastReadNum >= curNum){
@@ -155,20 +164,21 @@
                   duration: 4000
                 });
               }else{
-                this.getPrice()//调用获取水量水费方法，以下同上
+                type===1? this.getPrice(params) : this.getCheck(params)
               }
             }else{//当选择字轮翻页时，上次读数一定比本次读数大，若上次读数为0，第一次就不予处理
               this.param.IsPage = true
               let lastWater = this.currentContract.LastYield //上次水量
               let totalNum ='',curWater = 0;//根据上次读数位数计算totalNum，本次水量
+              let lastRead = this.currentContract.LastReadNum//上次读书
+
               for(let i=0;i<lastRead.length;i++){
                 totalNum += "9"
               }
-              let lastRead = this.currentContract.LastReadNum//上次读书
               if(lastRead>0&&lastRead>curNum){
                 curWater = totalNum - lastRead + curNum
 
-                if(checks.indexOf('异常倍率')!='-1'){//上次读数大于0，且勾选了异常倍率时候，进行本次预估倍率计算
+                if(this.checks.indexOf('异常倍率')!='-1'){//上次读数大于0，且勾选了异常倍率时候，进行本次预估倍率计算
                   let magnificate = curWater/lastWater //公式（本次水量/上次水量）
                   if(magnificate > this.magnification){//若预估结果大于等于用户所选倍率
                     this.$confirm("本次水量"+ curWater +"方，为上次水量"+ lastWater +"方的"+ magnificate +"倍，确认继续录入？", "提示", {
@@ -178,7 +188,7 @@
                       customClass: "warningBox",
                       showClose: false
                     }).then(() => {
-                      this.getPrice()
+                      type===1? this.getPrice(params) : this.getCheck(params)
                     })
                   }
                 }
@@ -205,10 +215,11 @@
           });
         }
       },
-      getPrice(){
-        WaterYieldPricePredict(this.param).then(res => {
+      getPrice(param){
+        alert('预估')
+        WaterYieldPricePredict(param).then(res => {
           if (res.code ==0 ) {
-
+              this.waterPriceData = res.data
           } else {
             this.$message({
               message: res.message,
@@ -223,7 +234,6 @@
       var _this = this;
       Bus.$on('msg',(e) =>{//从兄弟组件接收抄表计划名称
         _this.meterPlan = e
-        _this.param.SA_MeterRecord_Id = e.Id
       })
     }
   }
--- src/views/businessManagement/tableSeting/components/Schedule.vue
+++ src/views/businessManagement/tableSeting/components/Schedule.vue
@@ -1,42 +1,299 @@
 <template>
   <el-dialog
-    class="forms"
+    class="forms_dialog"
     :close-on-click-modal="false"
     top="5vh"
     :visible.sync="dialogVisible"
     width="95%">
-    <el-row :getter="18">
+    <el-row :gutter="18">
       <el-col :span="12">
-        sd
+        <h2>临时表册</h2>
+        <div class="forms_box">
+           <el-form
+             :inline="true"
+             :model="rbdp"
+             class="head-search-form form-inline-small-input"
+             size="small"
+             label-width="100px">
+             <el-form-item label="水厂：">
+               <el-select v-model="rbdp.SA_WaterFactory_Id" placeholder="请选择" size="small">
+                 <el-option label="全部" value="-1"></el-option>
+                 <el-option v-for="(item,index) in operatorArray" :key="index" :label="item.Name" :value="item.Id" />
+               </el-select>
+             </el-form-item>
+             <el-form-item label="所属区域：">
+               <el-select v-model="rbdp.sA_UserArea_Id" placeholder="请选择" size="small">
+                 <el-option label="全部" value="-1"></el-option>
+                 <el-option v-for="(item,index) in operatorArray" :key="index" :label="item.Name" :value="item.Id" />
+               </el-select>
+             </el-form-item>
+             <el-form-item label="水表类型：">
+               <el-select v-model="rbdp.BookTypeKey" placeholder="请选择" size="small">
+                 <el-option label="全部" value="-1"></el-option>
+                 <el-option v-for="(item,index) in operatorArray" :key="index" :label="item.Name" :value="item.Id" />
+               </el-select>
+             </el-form-item>
+             <el-form-item  label="用户：">
+               <el-select v-model="rbdp.ecqt" placeholder="请选择" style="width: 80px;float: left">
+                 <el-option label="编号" value="1"></el-option>
+                 <el-option label="姓名" value="2"></el-option>
+                 <el-option label="简码" value="3"></el-option>
+               </el-select>
+               <el-input v-model="rbdp.Customer" maxlength="20" placeholder="(长度1-30)" style="width: 180px;float: left"/>
+             </el-form-item>
+             <el-form-item label=""><el-button type="primary" size="small" class="cl-search" @click="searchFun"><i class="icon iconfont">&#xe694;</i> 搜索</el-button></el-form-item>
+           </el-form>
+           <div class="cl-operation1 clearfix">
+             <el-button type="primary" size="small" class="cl-search fr cl-color1" @click="setCustomData(1)"><i class="icon iconfont">&#xe678;</i> 表格自定义</el-button>
+             <el-button type="success" size="small" class="cl-search fr" @click="exportExcel"><i class="icon iconfont">&#xe683;</i> 导出Excel</el-button>
+           </div>
+           <!--自定义组建 s-->
+           <CustomTable ref="formChilds"></CustomTable>
+           <!--自定义组建 e-->
+           <!--列表组建 s-->
+           <el-table id="table2" :data="tableData" :height="tableHeight" style="width: 100%" border @sort-change="sortChanges">
+             <el-table-column
+               type="index"
+               label="序号"
+               width="80"
+               align="center"
+               fixed="left"/>
+             <template v-for="(item ,index) in tableHead" >
+               <el-table-column
+                 v-if="item.IsFreeze"
+                 :key="index"
+                 min-width="200px"
+                 sortable='custom'
+                 :prop="item.ColProp"
+                 :align="item.Position"
+                 :label="item.ColDesc"
+                 :fixed= "item.Freeze"/>
+               <el-table-column
+                 v-else
+                 :key="index"
+                 min-width="200px"
+                 sortable='custom'
+                 :prop="item.ColProp"
+                 :align="item.Position"
+                 :label="item.ColDesc"/>
+             </template>
+           </el-table>
+           <pagination
+             v-show="total>0"
+             :total="total"
+             :page.sync="rbdp.page"
+             :limit.sync="rbdp.limit"
+             @pagination="searchFun"/>
+           <!--列表组建 e-->
+         </div>
       </el-col>
       <el-col :span="12">
-        fg
+        <h2>表册</h2>
+        <div class="forms_box">
+          <el-form
+            :inline="true"
+            :model="rbdp"
+            class="head-search-form form-inline-small-input"
+            size="small"
+            label-width="100px"
+            @submit.native.prevent>
+            <el-form-item label="水厂：">
+              <el-select v-model="rbdp.SA_WaterFactory_Id" placeholder="请选择" size="small"  @keyup.enter.native="">
+                <el-option label="全部" value="-1"></el-option>
+                <el-option v-for="(item,index) in operatorArray" :key="index" :label="item.Name" :value="item.Id" />
+              </el-select>
+            </el-form-item>
+            <el-form-item label="抄表员：">
+              <el-select v-model="rbdp.meterReaderId" placeholder="请选择" size="small"  @keyup.enter.native="">
+                <el-option label="全部" value="-1"></el-option>
+                <el-option v-for="(item,index) in operatorArray" :key="index" :label="item.Name" :value="item.Id" />
+              </el-select>
+            </el-form-item>
+            <el-form-item  label="用户：">
+              <el-select v-model="rbdp.ecqt" placeholder="请选择" style="width: 80px;float: left">
+                <el-option label="编号" value="1"></el-option>
+                <el-option label="姓名" value="2"></el-option>
+                <el-option label="简码" value="3"></el-option>
+              </el-select>
+              <el-input v-model="rbdp.Customer" maxlength="20" placeholder="(长度1-30)" @keyup.enter.native="" style="width: 180px;float: left"/>
+            </el-form-item>
+            <el-form-item label="表册：">
+              <el-select v-model="rbdp.ComboBoxListByWaterFactory" placeholder="请选择" size="small"  @keyup.enter.native="">
+                <el-option label="全部" value="-1"></el-option>
+                <el-option v-for="(item,index) in operatorArray" :key="index" :label="item.Name" :value="item.Id" />
+              </el-select>
+            </el-form-item>
+            <el-form-item label=""><el-button type="primary" size="small" class="cl-search" @click=""><i class="icon iconfont">&#xe694;</i> 搜索</el-button></el-form-item>
+          </el-form>
+          <div class="cl-operation1 clearfix">
+            <el-button type="primary" size="small" class="cl-search fr cl-color1" @click="setCustomData(2)"><i class="icon iconfont">&#xe678;</i> 表格自定义</el-button>
+            <el-button type="success" size="small" class="cl-search fr" @click="exportExcel"><i class="icon iconfont">&#xe683;</i> 导出Excel</el-button>
+          </div>
+          <!--自定义组建 s-->
+          <CustomTable ref="formChilds2"></CustomTable>
+          <!--自定义组建 e-->
+          <!--列表组建 s-->
+          <el-table id="table3" :data="tableData2" :height="tableHeight3" style="width: 100%" border @sort-change="sortChanges">
+            <el-table-column
+              type="index"
+              label="序号"
+              width="80"
+              align="center"
+              fixed="left"/>
+            <template v-for="(item ,index) in tableHead2" >
+              <el-table-column
+                v-if="item.IsFreeze"
+                :key="index"
+                min-width="200px"
+                sortable='custom'
+                :prop="item.ColProp"
+                :align="item.Position"
+                :label="item.ColDesc"
+                :fixed= "item.Freeze"/>
+              <el-table-column
+                v-else
+                :key="index"
+                min-width="200px"
+                sortable='custom'
+                :prop="item.ColProp"
+                :align="item.Position"
+                :label="item.ColDesc"/>
+            </template>
+          </el-table>
+          <pagination
+            v-show="total>0"
+            :total="total"
+            :page.sync="rbdp.page"
+            :limit.sync="rbdp.limit"
+            @pagination="searchFun"/>
+          <!--列表组建 e-->
+        </div>
       </el-col>
     </el-row>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="dialogVisible = false" size="small">取 消</el-button>
+    </div>
   </el-dialog>
 </template>
 
 <script>
+  import CustomTable from '@/components/CustomTable/index'//自定义组建
+  import Pagination from '@/components/Pagination/index'//分页
 
+  /*0000010  0000011*/
   export default {
     name: "Schedule",
+    components: { CustomTable, Pagination },
     data() {
       return {
         dialogVisible: false,
+        operatorArray:[],
+        rbdp:{
+          SA_WaterFactory_Id:'',//水厂
+          sA_UserArea_Id:'-1',//区域
+          BookTypeKey:'-1',//水表类型
+          ecqt:'',//用户类型
+          Customer:'',//用户value
+          meterReaderId:'-1',//抄表员
+          ComboBoxListByWaterFactory:'-1',//表册
+          limit: 0,
+          page: 0,
+          sort: "",
+          filed: "",
+          tableId: ""
+        },
+        total:0,
+        tableData:[],
+        tableData2:[],
+        customHeight:'',
+        customHeight2:'',
+        tableHeight:null,
+        tableHeight3:null,
+        checksData: [],
+        checksData2: [],
       }
     },
     methods: {
-
+      setCustomData(type) {//表格自定义方法
+        if(type==1){
+          this.$refs.formChilds.isCustom = !this.$refs.formChilds.isCustom
+          this.customHeight = this.$refs.formChilds.isCustom
+        }else{
+          this.$refs.formChilds2.isCustom = !this.$refs.formChilds2.isCustom
+          this.customHeight2 = this.$refs.formChilds2.isCustom
+        }
+      },
+      sortChanges({prop, order }){//排序
+        this.rbdp.filed = prop
+        this.rbdp.sort=order=='ascending'?'ASC':(order=='descending'?'DESC':'')
+        if(this.tableData.length>0){
+          this.rbdp.page = 1
+          this.searchFun()
+        }
+      },
+      exportExcel(){},
+      searchFun(){},
+      getTableInfo(){
+        let _this = this
+        _this.$nextTick(()=>{
+          _this.$refs.formChilds.GetTable('0000010');//临时表册
+          _this.checksData = this.$refs.formChilds.checkData//获取自定义字段中选中了字段
+          _this.$refs.formChilds2.GetTable('0000011');//表册
+          _this.checksData2 = this.$refs.formChilds2.checkData//获取自定义字段中选中了字段
+          this.tableHeight = document.documentElement.clientHeight - document.getElementById('table2').offsetTop - 200
+          this.tableHeight3 = document.documentElement.clientHeight - document.getElementById('table3').offsetTop - 200
+        })
+      }
     },
-    mounted() {
-
+    watch: {
+      customHeight() {//获取自定义模块高度左侧
+        let self = this
+        self.$nextTick(() => {//左侧列表高度
+          this.tableHeight = document.documentElement.clientHeight - document.getElementById('table2').offsetTop - 200
+        })
+      },
+      customHeight2() {//获取自定义模块高度右侧
+        let self = this
+        self.$nextTick(() => {//右侧列表高度
+          this.tableHeight3 = document.documentElement.clientHeight - document.getElementById('table3').offsetTop - 200
+        })
+      }
+    },
+    computed: {
+      tableHead: function() {//获取左侧表头信息
+        const arrayHead = []
+        const data = this.checksData
+        for (let i = 0; i < data.length; i++) { // 过滤选中列
+          if (data[i].IsCheck) {
+            arrayHead.push(data[i])
+          }
+        }
+        return arrayHead
+      },
+      tableHead2: function() {//获取右侧表头信息
+        const arrayHead = []
+        const data = this.checksData2
+        for (let i = 0; i < data.length; i++) { // 过滤选中列
+          if (data[i].IsCheck) {
+            arrayHead.push(data[i])
+          }
+        }
+        return arrayHead
+      }
     }
   }
 </script>
 <style lang="scss">
-  .forms{
+  .forms_dialog{
+    .el-row{padding: 0 20px}
     .el-dialog__header {background-color: #EBEBEB !important;padding: 0 !important;}
     .el-dialog__headerbtn{display: none}
     .el-dialog__body{background: #EBEBEB;padding: 18px;}
+    .el-dialog__footer{background: #EBEBEB;padding: 0 0 10px 0;}
+    h2{margin: 0 0 0 0;font: normal 14px/40px 'Microsoft YaHei';background: #006663;color: #fff;padding: 0 30px}
+    .forms_box{
+      .cl-color1{margin-left: 10px;}
+      background: #fff;position: relative;padding: 25px 20px 0 20px;
+    }
   }
 </style>
