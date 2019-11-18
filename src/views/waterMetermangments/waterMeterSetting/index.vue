cfe6a701749d27fd4e27fa3b37ef6fdf081f5985
ganxu <1228975494@qq.com>
2019-11-18 18:02:16 +0800

    水表管理

---
+++ src/views/waterMetermangments/waterMeterSetting/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="section-container">
+    <div class="section-full-container">
+      <h1>水表管理</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "WaterMeterSetting"
+};
+</script>   
