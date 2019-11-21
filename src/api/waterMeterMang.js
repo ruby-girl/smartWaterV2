060b3bc4c11a8298c832f43cd34ee143a38ebc52
ganxu <1228975494@qq.com>
2019-11-21 17:16:27 +0800

    水表管理

--- src/api/waterMeterMang.js
+++ src/api/waterMeterMang.js
@@ -2,15 +2,29 @@ import request from '@/utils/request'
 
 export function searJXMeterWater(data) {//机械表查询
   return request({
-    url:"/WaterMeter/GetWaterMeter1List",
+    url: "/WaterMeter/GetWaterMeter1List",
     method: 'post',
     data
   })
 }
 export function searJXHisWater(data) {//机械表历史
-return request({
-    url:"/WaterMeter/GetWaterMeter1MeterReadList",
+  return request({
+    url: "/WaterMeter/GetWaterMeter1MeterReadList",
     method: 'post',
     data
-})
+  })
+}
+export function editJXHisWater(data) {//获取机械编辑信息
+  return request({
+    url: "/WaterMeter/GetWaterMeter1BlById",
+    method: 'post',
+    data
+  })
+}
+export function saveJXHisWater(data) {//编辑机械编辑信息
+  return request({
+    url: "/WaterMeter/WaterMeter1UpDate",
+    method: 'post',
+    data
+  })
 }
--- src/views/waterMetermangments/waterMeterSetting/conmponents/intercomponents/WaterMeterHis.vue
+++ src/views/waterMetermangments/waterMeterSetting/conmponents/intercomponents/WaterMeterHis.vue
@@ -1,34 +1,43 @@
 <template>
-  <div class="main-padding-20-y" id="table">
-    <el-table
-      :key="tableKey"
-      :data="tableData"
-      border
-      fit
-      :height="tableHeight"
-      style="width: 100%;"
-      :header-cell-style="{'background-color': '#F0F2F5'}"
-      :cell-style="{'padding':'7px 0'}"
-    >
-      <el-table-column type="index" width="50"></el-table-column>
-      <el-table-column fixed prop="CustomerName" label="姓名" width="150"></el-table-column>
-      <el-table-column fixed prop="WaterMeterNo" label="水表编号" width="150"></el-table-column>
-      <el-table-column fixed prop="WaterMeterStyleStr" label="水表样式" width="150"></el-table-column>
-      <el-table-column fixed prop="MeterDiameter" label="口径" width="150"></el-table-column>
-      <el-table-column fixed prop="StarReadNum" label="起始读数" width="150"></el-table-column>
-      <el-table-column fixed prop="ReadNum" label="当前读数" width="150"></el-table-column>
-      <el-table-column fixed prop="ReadDate" label="最近一次抄表日期" width="150"></el-table-column>
-    </el-table>
-    <pagination
-      v-show="total>0"
-      :total="total"
-      :page.sync="wachMeterData.page"
-      :limit.sync="wachMeterData.limit"
-    />
-  </div>
+  <el-table
+    :data="hisData"
+    height="360"
+    border
+    style="width: 100%;"
+    :header-cell-style="{'background-color': '#F0F2F5'}"
+    :cell-style="rowClsass"
+  >
+    <el-table-column type="index" width="50"></el-table-column>
+    <el-table-column prop="CustomerName" label="姓名" width="150"></el-table-column>
+    <el-table-column prop="WaterMeterNo" label="水表编号" width="150"></el-table-column>
+    <el-table-column prop="WaterMeterStyleStr" label="水表样式" width="150"></el-table-column>
+    <el-table-column prop="MeterDiameter" label="口径" width="150"></el-table-column>
+    <el-table-column prop="StarReadNum" label="起始读数" width="150"></el-table-column>
+    <el-table-column prop="ReadNum" label="当前读数" width="150"></el-table-column>
+    <el-table-column prop="ReadDate" label="最近一次抄表日期" width="200"></el-table-column>
+  </el-table>
 </template>
 <script>
+import Pagination from "@/components/Pagination/index"; //分页
 export default {
-  name: "WaterMeterHis"
+  name: "WaterMeterHis",
+  components: { Pagination },
+  props: {
+    hisData: {
+      default: [],
+      type: Array
+    }
+  },
+  methods:{
+  rowClsass(){
+
+    return 'text-align: center;padding:7px 0'
+  }
+  }
 };
 </script>
+<style scoped>
+    .cell{
+      text-align: center
+    }
+</style>
