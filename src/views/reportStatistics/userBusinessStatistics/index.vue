b7738d0c2aa72b4833bb6d05d22105045a616b94
ganxu <1228975494@qq.com>
2020-01-15 18:02:12 +0800

    报表统计

--- src/views/reportStatistics/userBusinessStatistics/index.vue
+++ src/views/reportStatistics/userBusinessStatistics/index.vue
@@ -12,6 +12,8 @@
             border
             fit
             :height="tableHeight"
+             :summary-method="getSummaries"
+    show-summary
             style="width: 100%;"
             :header-cell-style="{'background-color': '#F0F2F5'}"
           >
@@ -20,7 +22,7 @@
             <el-table-column prop="CreateUserName" label="操作员" align="center"></el-table-column>
             <el-table-column prop="AccountsNum" label="开户数" align="center">
               <template slot-scope="scope">
-                <span class="colorStyle" @click="AccountsNum">{{scope.row.AccountsNum}}</span>
+                <span class="colorStyle" @click="AccountsNum(scope.row.CreateUserId)">{{scope.row.AccountsNum}}</span>
               </template>
             </el-table-column>
             <el-table-column prop="TransferNum" label="过户数" align="center">
@@ -55,6 +57,26 @@
           </el-table>
         </div>
       </div>
+      <el-dialog
+      :title="dialTitle"
+      :visible.sync="viewWaterHistory"
+      top="20vh"
+      width="836px"
+      hight="432px"
+      center
+      :close-on-click-modal="false"
+    >
+       <accounts-num  :meterReadListParam="selectHead" :hisData="accounData" />
+
+      <pagination
+        v-show="histotal>0"
+        :total="histotal"
+        :page.sync="selectHead.page"
+        :limit.sync="selectHead.limit"
+        @pagination=""
+      />
+    </el-dialog>
+    
     </div>
   </div>
 </template>
@@ -62,6 +84,8 @@
 <script>
 import SelectHead from "./components/SelectHead";
 import SearchTips from "@/components/SearchTips/index";
+import AccountsNum from "./components/AccountsNum";//开户
+import Pagination from "@/components/Pagination/index"; //分页
 import { delTips, getText, pushItem } from "@/utils/projectLogic"; //搜索条件面包屑
 import {
   GetReportBusiness,
@@ -75,66 +99,108 @@ import {
 } from "@/api/reports";
 export default {
   name: "userBusinessStatistics",
-  components: { SelectHead, SearchTips },
+  components: { SelectHead, SearchTips,AccountsNum,Pagination  },
   data() {
     return {
       selectHead: {
-        waterFactoryId: "-1", //水厂
+        page:1,limit:10,
+        waterFactoryId: "", //水厂
         waterFactoryName: "", //水厂
         createUserId: "-1", //操作人
-        createUserName: "", //操作人
+        createUserName: "全部", //操作人
         userType: -1, //用户类型
-        userTypeName: "",
+        userTypeName: "全部",
         waterMeterType: -1, //水表类型
-        waterMeterTypeName: "", //水表类型
+        waterMeterTypeName: "全部", //水表类型
         createStartTime: "",
         createEndTime: ""
       },
+      detaileselectHead:{},
       tableHeight: null,
       tableData: [], //表格数据
       tipsData: [], //传入子组件的值
       tipsDataCopy: [], //表单变化的值
-      searchWidth: 1024
+      searchWidth: 1024,
+      histotal:0,
+      accounData:[],
+      viewWaterHistory:false,
+      dialTitle:""
     };
   },
   methods: {
     //开户
-    AccountsNum() {
+    AccountsNum(CreateUserId) {
+      this.selectHead.createUserId=CreateUserId
       GetReportOpen(this.selectHead).then(res => {
         console.log(res);
+        this.dialTitle="开户详情"
+        this.viewWaterHistory=true
+        this.accounData=res.data
+        this.histotal=res.count
       });
     },
     //过户
     TransferNum() {
       GetReportTransfer(this.selectHead).then(res => {
         console.log(res);
+        this.dialTitle="过户详情"
+
       });
     },
     //销户
     AccountCancellationNum() {
       GetReportAccount(this.selectHead).then(res => {
         console.log(res);
+        this.dialTitle="销户详情"
       });
     },
     //换表
     ChangeNum() {
       GetReportChange(this.selectHead).then(res => {
-        console.log(res);
+        console.log(res);this.dialTitle="换表详情"
       });
     },
     //升级
     UpgradeNum() {
       GetReportUpgrade(this.selectHead).then(res => {
         console.log(res);
+        this.dialTitle="升级详情"
       });
     },
     //低保户
     InsuredNum() {
       GetReportInsured(this.selectHead).then(res => {
         console.log(res);
+        this.dialTitle="低保户详情"
       });
     },
+    getSummaries(param) {
+        const { columns, data } = param;
+        const sums = [];
+        columns.forEach((column, index) => {
+          if (index === 0) {
+            sums[index] = '合计';
+            return;
+          }
+          const values = data.map(item => Number(item[column.property]));
+          if (!values.every(value => isNaN(value))) {
+            sums[index] = values.reduce((prev, curr) => {
+              const value = Number(curr);
+              if (!isNaN(value)) {
+                return prev + curr;
+              } else {
+                return prev;
+              }
+            }, 0);
+            sums[index] ;
+          } else {
+            sums[index] = '/';
+          }
+        });
 
+        return sums;
+      
+    },
     delTips(val) {
       if (val == "dateArr") {
         //当返回的model 为时间数组  置空 时间
@@ -177,7 +243,9 @@ export default {
     //查询
     searchTableList() {
       GetReportBusiness(this.selectHead).then(res => {
+          // this.detaileselectHead=
         this.tableData = res.data;
+        this.tableData.pop()
         this.tipsData = pushItem(this.tipsDataCopy);
       });
     }
